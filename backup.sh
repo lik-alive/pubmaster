ba80f1143a900494c233b5000f9ccb15d073cc26
lik-alive <kirshdv@gmail.com>
2021-09-10 21:05:21 +0400

    r

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
-echo 'pubmaster backup started'
-ROOT=$1/pubmaster
+title=pubmaster
+
+echo $title backup started
+ROOT=$1/$title
 mkdir $ROOT
 
 # Backup web files
 WEBPATH=$ROOT/web
 mkdir $WEBPATH
-rsync -a --info=progress2 . $WEBPATH --exclude .git --exclude "files/temp"
+#rsync -a --info=progress2 . $WEBPATH --exclude .git --exclude "files/temp"
+tar cf - --exclude ".git" --exclude "files/temp" . | pv -s $(du -sb . | awk '{print $1}') | gzip > $WEBPATH/$title.tar.gz
 
 # Backup db
 DBPATH=$ROOT/db
 mkdir $DBPATH
 mysqldump -u$2 -p$3 --databases pubdb > $DBPATH/dump.sql 2> /dev/null
-echo 'pubmaster backup finished'
+
+echo $title backup finished
