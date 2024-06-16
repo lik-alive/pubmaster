bfada99f9bf6b8c21c36248376fa2c35c2863324
lik-alive <kirshdv@gmail.com>
2024-06-17 03:30:38 +0400

    minor fixes

--- wp-content/themes/simplified/footer.php
+++ wp-content/themes/simplified/footer.php
@@ -3,7 +3,7 @@
 
 <footer class='footer text-center text-white mt-4 pt-2'>
 	<i>Архив публикаций коллектива 723-й лаборатории</i> <br/>
-	<div class='text-right pr-2 small'>©2020-2016 LIK</div>
+	<div class='text-right pr-2 small'>©<?php echo date('Y') ?>-2016 LIK</div>
 </footer>
 
 </div>
